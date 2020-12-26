e62f743e29f13f1941990d9ca9b70fb9fa410206
mfshiu <avatar.xu@gmail.com>
2020-12-27 07:25:47 +0800

    evaluate gpu

--- train.py
+++ train.py
@@ -8,79 +8,7 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import random
 import sys
-
-
-class Siamese(nn.Module):
-
-    def __init__(self, use_gpu):
-        super(Siamese, self).__init__()
-        self.use_gpu = use_gpu
-
-        # A simple two layer convolution followed by three fully connected layers should do
-
-        self.cnn = nn.Sequential(
-            nn.Conv2d(3, 64, 3, 1, 1),  # [64, 128, 128]
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2, 0),  # [64, 64, 64]
-
-            nn.Conv2d(64, 128, 3, 1, 1),  # [128, 64, 64]
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2, 0),  # [128, 32, 32]
-
-            nn.Conv2d(128, 256, 3, 1, 1),  # [256, 32, 32]
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-
-        )
-
-        self.fc = nn.Sequential(
-            nn.Linear(256, 128),
-            nn.ReLU(),
-            nn.Linear(128, 32),
-            nn.ReLU(),
-            nn.Linear(32, 2),
-        )
-
-    def forward_once(self, x):
-        out = self.cnn(x)
-        # print(out.shape)
-        out = out.view(-1, 256)
-        # print(out.shape)
-        return self.fc(out)
-
-    def forward(self, x, y):
-
-        # doing the forwarding twice so as to obtain the same functions as that of twin networks
-
-        out1 = self.forward_once(x)
-        out2 = self.forward_once(y)
-
-        return out1, out2
-
-    def evaluate(self, x, y):
-        if self.use_gpu:
-            m = to.tensor(1.0, dtype=to.float32).cuda()
-            if type(m) != type(x):
-                x = to.tensor(x, dtype=to.float32, requires_grad=False).cuda()
-            if type(m) != type(y):
-                y = to.tensor(y, dtype=to.float32, requires_grad=False).cuda()
-        else:
-            m = to.tensor(1.0, dtype=to.float32).cpu()
-            if type(m) != type(x):
-                x = to.tensor(x, dtype=to.float32, requires_grad=False).cpu()
-            if type(m) != type(y):
-                y = to.tensor(y, dtype=to.float32, requires_grad=False).cpu()
-
-        x = x.view(-1, 3, img_size, img_size)
-        y = y.view(-1, 3, img_size, img_size)
-
-        with to.no_grad():
-
-            out1, out2 = self.forward(x, y)
-
-            return nn.functional.pairwise_distance(out1, out2)
+from model import Siamese
 
 
 class ContrastiveLoss(nn.Module):
@@ -195,9 +123,9 @@ if __name__ == '__main__':
     dataset_dir = "./dataset/train"
 
     if use_gpu:
-        siam = Siamese(use_gpu).cuda()
+        siam = Siamese().cuda()
     else:
-        siam = Siamese(use_gpu).cpu()
+        siam = Siamese().cpu()
     Criterion = ContrastiveLoss()
     Optimizer = to.optim.Adam(siam.parameters(),lr = 0.01 )
 
