e81897a344209dd00fc240f6dd21e03956bf2f65
mfshiu <avatar.xu@gmail.com>
2020-12-29 18:09:30 +0800

    train-old2.py

--- 1214_siamese_net_dog_cnn.py
+++ 1214_siamese_net_dog_cnn.py
@@ -23,6 +23,7 @@ import matplotlib.pyplot as plt
 import torch as to
 import torch.nn as nn
 from torch.utils.data import DataLoader, Dataset
+from model import Siamese
 
 """## Training the Siamese Netwrok"""
 
@@ -190,81 +191,6 @@ class SiamDataset(Dataset):
         # here I gave a smaller length than the real dataset's length so that the train-20 can be faster
             
         return 200
-
-"""##### The Model Definition of Siamese Network
-
-Here unlike as stated in the paper I have used a single network and trained the dataset. This can be done as both the layers are completely tied even in the train-20.
-"""
-
-class Siamese(nn.Module):
-    
-    def __init__(self):
-        super(Siamese,self).__init__()
-        
-        # A simple two layer convolution followed by three fully connected layers should do
-        
-        self.cnn = nn.Sequential(
-            nn.Conv2d(3, 64, 3, 1, 1),  # [64, 128, 128]
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2, 0),      # [64, 64, 64]
-
-            nn.Conv2d(64, 128, 3, 1, 1), # [128, 64, 64]
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2, 0),      # [128, 32, 32]
-
-            nn.Conv2d(128, 256, 3, 1, 1), # [256, 32, 32]
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-        )
-        
-        self.fc = nn.Sequential(
-            nn.Linear(256, 128),
-            nn.ReLU(),
-            nn.Linear(128, 32),
-            nn.ReLU(),
-            nn.Linear(32, 2)
-        )
-        
-        
-    def forward_once(self,x):
-        out = self.cnn(x)
-        #print(out.shape)
-        out = out.view(-1, 256)
-        #print(out.shape)
-        return self.fc(out)
-    
-    def forward(self, x, y):    
-        
-        # doing the forwarding twice so as to obtain the same functions as that of twin networks
-        
-        out1 = self.forward_once(x)
-        out2 = self.forward_once(y)
-        
-        return out1, out2
-    
-    
-    def evaluate(self, x, y):
-        
-        # this can be used later for evalutation
-        
-        m = to.tensor(1.0, dtype=to.float32).cuda()
-        
-        if type(m) != type(x):
-            x = to.tensor(x, dtype = to.float32, requires_grad = False).cuda()
-            
-        if type(m) != type(y):
-            y = to.tensor(y, dtype = to.float32, requires_grad = False).cuda()
-        
-        x = x.view(-1,3,img_size,img_size)
-        y = y.view(-1,3,img_size,img_size)
-        
-        with to.no_grad():
-            
-            out1, out2 = self.forward(x, y)
-            
-            return nn.functional.pairwise_distance(out1, out2)
 
 """##### Loss Function Definition for Siamese Network
 
